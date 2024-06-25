aaf29ef93bd16b8120149ff750f0079de1d92e48
Raafey khan <113880768+Raafey-Khan@users.noreply.github.com>
2024-06-25 18:18:37 +0530

    early back

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,10 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+   
   ],
   theme: {
     extend: {
-      borderRadius: {
-        'bl-custom': '1rem', // replace 1rem with your desired value
-      },
-      colors: {
-        custom: '#f6f6f6',
-        raimbow: '#E4F3F4',
-      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
